21d154bb09612fc4ed9b5a9c19153cbe4c3b413e
Gilbert Jung <83274792+Gilbert9172@users.noreply.github.com>
2024-11-05 13:12:00 +0900

    fix: 에러메시지 prefix 대소문자 수정

--- src/main/java/lotto/error/ErrorMessage.java
+++ src/main/java/lotto/error/ErrorMessage.java
@@ -5,8 +5,8 @@ public class ErrorMessage {
     private ErrorMessage() {
     }
 
-    public static final String SHOULD_NOT_BE_NULL = "[Error] Null이여서는 안됩니다.";
-    public static final String AMOUNT_SHOULD_NOT_BE_MINUS = "[Error] 금액은 음수가 될 수 없습니다.";
-    public static final String INVALID_UNIT_AMOUNT = "[Error] 1000원 단위로 입력해주세요.";
-    public static final String DUPLICATED_LOTTO_NUMBER = "[Error] 중복된 로또 번호가 있습니다.";
+    public static final String SHOULD_NOT_BE_NULL = "[ERROR] Null이여서는 안됩니다.";
+    public static final String AMOUNT_SHOULD_NOT_BE_MINUS = "[ERROR] 금액은 음수가 될 수 없습니다.";
+    public static final String INVALID_UNIT_AMOUNT = "[ERROR] 1000원 단위로 입력해주세요.";
+    public static final String DUPLICATED_LOTTO_NUMBER = "[ERROR] 중복된 로또 번호가 있습니다.";
 }
